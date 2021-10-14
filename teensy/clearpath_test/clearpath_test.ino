d4d8742c564f4f37f3946a0a2fb898e151ca9049
mbrooker <marcbrooker@gmail.com>
2021-10-14 11:05:46 -0700

    Simplify filtering logic by switching to simple FIR filter

--- teensy/clearpath_test/clearpath_test.ino
+++ teensy/clearpath_test/clearpath_test.ino
@@ -2,12 +2,15 @@
 
 // Run the inner loop every this number of millis.
 #define LOOP_EVERY_MS 100
-
-#define RPM_FILTER_BUCKETS 5
-
 #define ANALOG_WRITE_RES 10
+#define RPM_FILTER_BUCKETS 7
+
 // Configured max motor RPM. This is set in the clearpath motor's configuration, done ahead of time via USB
 #define CP_MOTOR_MAX_RPM 1000
+// Leadscrew pitch, in MM per full revolution
+#define LEADSCREW_PITCH 2.0
+// Powerfeed gear ratio, as a ratio of the leadscrew pitch
+#define POWERFEED_RATIO 0.18
 
 // Pin allocations
 const int pin_cp_enable = 20;
@@ -17,7 +20,6 @@ const int pin_cp_speed = 22;
 const int pin_encoder_a = 1;
 const int pin_encoder_b = 2;
 const int pin_encoder_z = 3;
-Encoder enc(pin_encoder_a, pin_encoder_b);
 
 enum motor_direction {
   cw,
@@ -26,7 +28,13 @@ enum motor_direction {
 
 enum feedrate_mode {
   per_minute, // G98
-  per_rev // G99
+  per_rev, // G99
+};
+
+// There are two ways to make the carriage move with the lead screw: the half-nut (for threading), and power feed (for turning)
+enum feedrate_type {
+  half_nut,
+  power_feed,
 };
 
 typedef struct {
@@ -35,15 +43,27 @@ typedef struct {
 } gcode_verb;
 
 typedef struct {
-  long rpm_filter_accum = 0;
-  long enc_bucket_start = 0;
   long pos;
   long buckets[RPM_FILTER_BUCKETS];
   long bucket_pointer = 0;
   double rpm = 0;
 } encoder_state;
 
+typedef struct {
+  double feed;
+  feedrate_mode feed_mode;
+  feedrate_type feed_type;
+} config_t;
+
 long last_loop_start = 0;
+encoder_state enc_state;
+Encoder enc(pin_encoder_a, pin_encoder_b);
+
+config_t conf {
+  0.05,
+  per_rev,
+  power_feed,
+};
 
 void setup() {
   Serial.begin(9600);
@@ -60,15 +80,42 @@ void serialEvent() {
   read_gcode_from_serial();
 }
 
+double calculate_leadscrew_rpm(double spindle_rpm, config_t *conf) {
+  spindle_rpm = abs(spindle_rpm);
+  if (conf->feed_type == half_nut) {
+    return spindle_rpm * conf->feed / LEADSCREW_PITCH;
+  } else if (conf->feed_type == power_feed) {
+    // Power cross-feed mode
+    return spindle_rpm * conf->feed / (LEADSCREW_PITCH * POWERFEED_RATIO);
+  } else {
+    return 0;
+  }
+}
+
+motor_direction rpm_to_leadscrew_dir(double spindle_rpm) {
+  if (spindle_rpm > 0) {
+    return cw;
+  } else {
+    return ccw;
+  }
+}
+
 void loop() {
   long now = millis();
-
-  move_clearpath(10, cw);
-
+  encoder_read_loop(now, &enc_state);
+  double leadscrew_rpm = calculate_leadscrew_rpm(enc_state.rpm, &conf);
+  motor_direction leadscrew_dir = rpm_to_leadscrew_dir(enc_state.rpm);
+  move_clearpath(leadscrew_rpm, leadscrew_dir);
   long end = millis();
 
   // Delay for the amount of time it takes to run this inner loop at a constant rate
   long delay_for = constrain(LOOP_EVERY_MS + now - end, 0, LOOP_EVERY_MS);
-  Serial.println(delay_for);
+  Serial.print(delay_for);
+  Serial.print(" ");
+  Serial.print(enc_state.rpm);
+  Serial.print(" ");
+  Serial.print(leadscrew_rpm);
+  Serial.print(" ");
+  Serial.println(leadscrew_dir);
   delay(delay_for);
 }
--- teensy/clearpath_test/encoder.ino
+++ teensy/clearpath_test/encoder.ino
@@ -1,40 +1,31 @@
-#define RPM_FILTER_BUCKETS 5
-#define RPM_FILTER_MS 1000
-#define RPM_FILTER_A0 0.9
-// Filter gain for the bucket filter, see gain.py for how its calculated from RPM_FILTER_BUCKETS and RPM_FILTER_A0
-#define RPM_FILTER_GAIN 4.68
 
 #define TICKS_PER_REV (4 * 360.0)
 
-void update_rpm_filter(encoder_state *state) {
-  state->buckets[state->bucket_pointer] = state->rpm_filter_accum;
-  state->rpm_filter_accum = 0;
-  double ticks = 0;
-  double gain = 1.0;
-  for (int i = 0; i < RPM_FILTER_BUCKETS; i++) {
-    ticks += state->buckets[(state->bucket_pointer + i) % RPM_FILTER_BUCKETS] * gain;
-    gain *= RPM_FILTER_A0;
-  }
+// Simple windowed-sinc FIR filter, aiming for a cutoff frequency of 0.2pi
+double filter_coeffs[RPM_FILTER_BUCKETS] = { 0.094, 0.14, 0.173, 0.185, 0.173, 0.14, 0.094 };
 
-  // Move the bucket pointer over by one
+void rpm_filter_fir(encoder_state *state, long new_ticks) {
+  // Add the latest tick count to the bucket
+  state->buckets[state->bucket_pointer] = new_ticks;
+
+  // Apply the FIR filter to find the new RPM
+  double ticks = 0;
+  for (int i = 0; i < RPM_FILTER_BUCKETS; i++) {
+    ticks += state->buckets[(state->bucket_pointer + i) % RPM_FILTER_BUCKETS] * filter_coeffs[i];
+  }
+  double ticks_per_minute = ticks * 60000 / LOOP_EVERY_MS;
+  state->rpm = ticks_per_minute / TICKS_PER_REV;
+
   state->bucket_pointer -= 1;
   if (state->bucket_pointer < 0) {
     state->bucket_pointer = RPM_FILTER_BUCKETS - 1;
   }
-  // Now convert from ticks-per-period to RPM
-  ticks /= RPM_FILTER_GAIN;
-  double ticks_per_minute = ticks * 60000 / RPM_FILTER_MS;
-  state->rpm = ticks_per_minute / TICKS_PER_REV;
 }
 
 void encoder_read_loop(long now, encoder_state *state) {
-    long new_pos = enc.read();
+  long new_pos = enc.read();
+  long new_ticks = (new_pos - state->pos);
+  state->pos = new_pos;
 
-   if (new_pos != state->pos) {
-    state->rpm_filter_accum += (new_pos - state->pos);
-    state->pos = new_pos;
-  }
-  if ((now - state->enc_bucket_start) > RPM_FILTER_MS) {
-    update_rpm_filter(state);
-  }
+  rpm_filter_fir(state, new_ticks);
 }
